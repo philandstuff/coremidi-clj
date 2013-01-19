a3193470164c0d04ee04f9ec0987cc08ff539aa4
Philip Potter <philipgpotter@gmail.com>
2013-01-19 12:28:21 +0000

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject coremidi-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "CoreMIDI wrapper for Clojure"
+  :url "https://github.com/philandstuff/coremidi-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
