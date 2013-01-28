cc2f9806ca2c921c130c27d8298463702d3f385c
Philip Potter <philipgpotter@gmail.com>
2013-01-28 21:16:59 +0000

    decode note-on and note-off events

--- src/coremidi_clj/coremidi/decode.clj
+++ src/coremidi_clj/coremidi/decode.clj
@@ -8,10 +8,29 @@
      :controller controller
      :value      value}))
 
+(defn- decode-note-on [bytes]
+  (let [[status pitch velocity] bytes
+        channel (mod status 16)]
+    {:cmd      (if (zero? velocity) :note-off :note-on)
+     :chan     channel
+     :pitch    pitch
+     :velocity velocity}))
+
+(defn- decode-note-off [bytes]
+  (let [[status pitch velocity] bytes
+        channel (mod status 16)]
+    {:cmd      :note-off
+     :chan     channel
+     :pitch    pitch
+     :velocity velocity}))
+
 (defn decode-packet [packet]
   (let [status-byte   (first (:data packet))
         status-nybble (unchecked-divide-int status-byte 16)]
     (case status-nybble
+      0x8 (decode-note-off (:data packet))
+      0x9 (decode-note-on (:data packet))
       0xb (decode-controller-change (:data packet))
-      :unknown-midi-message)))
+      {:type :unknown-midi-message
+       :data (:data packet)})))
 
